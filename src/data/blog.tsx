d63a4b11909d75ba9a8f2175c6a542cba06d2b52
Samuel Soun <sounsamuel07@gmail.com>
2026-02-14 15:17:58 +0100

    feat: new blog - hack winti

--- src/data/blog.tsx
+++ src/data/blog.tsx
@@ -10,6 +10,16 @@ export type BlogPost = {
 
 export const blogPosts: BlogPost[] = [
   {
+    title: "Hack Winterthur 2026",
+    date: "February 2026",
+    excerpt:
+      "48 hours of Hack Winterthur, and I was right in the middle of it! 🚀 The weekend of February 6–8 was all about hacking and discussing ideas. Little sleep, lots of coffee, and an incredible number of exciting and inspiring encounters. I had the opportunity to work on the City of Winterthur's challenge at this hackathon. The goal was to use digital ideas and new services to sustainably advance Winterthur as a place to live, learn, and work. The highlight was the highly motivated team that came together that evening. Even the intense hours were genuinely fun with this crew. A huge thank you to the organizers for this amazing hackathon and to all the inspiring people I had the chance to meet. The 48 hours were intense, challenging, and at the same time incredibly motivating. This was definitely not my last hackathon!",
+    imageUrl: "/images/hack_winterthur_2026.png",
+    link: "https://hackwinterthur.ch/",
+    linkLabel: "Visit the Hack Winterthur",
+    tag: "Hackathon · Hack Winterthur · Stadt Winterthur · WinLab · Entrepreneurship Club Winterthur",
+  },
+  {
     title: "Communal elections in Suhr",
     date: "September 2025",
     excerpt:
@@ -17,7 +27,7 @@ export const blogPosts: BlogPost[] = [
     imageUrl: "/images/communal_election2025.jpeg",
     link: "https://www.linkedin.com/posts/samuel-soun-a49049247_gestern-fanden-die-kommunalwahlen-statt-activity-7378484150456098816-qcAC?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD0NdpkBtxc-g1A0OgCfGlAuZaa38wKRtW0",
     linkLabel: "Read on LinkedIn",
-    tag: "FDP.Die Liberalen Aargau Jungfreisinnige Aarau-Lenzburg-Kulm",
+    tag: "FDP.Die Liberalen Aargau · Jungfreisinnige Aarau-Lenzburg-Kulm",
   },
 ];
 
